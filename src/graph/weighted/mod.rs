24333f0f6f7241b0326c9db0d5bad0e96ef3d917
Manoel Menezes <manoel.menezes.jr@gmail.com>
2024-10-15 20:31:05 -0700

    Implement shortest path.

--- src/graph/weighted/mod.rs
+++ src/graph/weighted/mod.rs
@@ -1,4 +1,6 @@
+use priority_queue::PriorityQueue;
 use std::collections::HashMap;
+use std::cmp::Reverse;
 
 #[derive(Clone)]
 struct Edge {
@@ -13,6 +15,14 @@ impl Edge {
             u,
             v,
             weight,
+        }
+    }
+
+    fn to(&self, from: usize) -> usize {
+        if from == self.u {
+            self.v
+        } else {
+            self.u
         }
     }
 }
@@ -56,10 +66,41 @@ impl Graph {
         Ok(())
     } 
 
-    pub fn shortest_path(&self, from: usize, to: usize) -> Result<Vec<usize>, String> {
-        self.validate(from, to)?;
-        Ok(Vec::new())
+    pub fn shortest_path(&self, from: usize) -> Result<ShortestPathResult, String> {
+        if from >= self.n {
+            return Err(format!("From must be smaller than {}", self.n));
+        }
+        let mut dist = vec![usize::MAX; self.n];
+        dist[from] = 0;
+        let mut prev = vec![0; self.n];
+        let mut pq = PriorityQueue::new(); 
+        for n in 0..self.n {
+            if n != from {
+                pq.push(n, Reverse(usize::MAX));
+            }
+        }
+        pq.push(from, Reverse(0));
+        
+        while let Some((n, _)) = pq.pop() {
+            for e in self.nodes.get(&n).unwrap() {
+                let d = dist[n] + e.weight;
+                let to = e.to(n);
+                if d < dist[to] {
+                    prev[to] = n;
+                    dist[to] = d;
+                    pq.change_priority(&to, Reverse(d));
+                }
+            }
+        }
+        Ok(ShortestPathResult{prev: prev, dist: dist, from})
     }
 
+}
+
+#[derive(Debug)]
+pub struct ShortestPathResult {
+    pub prev: Vec<usize>,
+    pub dist: Vec<usize>,
+    pub from: usize,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rds::graph::Graph;
+use rds::graph::weighted::Graph as WeightedGraph;
 
 fn main() ->Result<(), String> {
     let mut graph = Graph::new(6);
@@ -6,13 +7,24 @@ fn main() ->Result<(), String> {
     graph.add_edge(1, 2)?;
     graph.add_edge(2, 3)?;
     graph.add_edge(4, 5)?;
-    
+
     let c = graph.are_connected(0, 3)?;
     println!("0 and 3 connected: {}", c);   
 
     let c = graph.are_connected(3, 4)?;
-    println!("3 and 4 connected: {}", c);
+    println!("3 and 4 connected: {}", c); 
 
+    let mut graph = WeightedGraph::new(5);
+    graph.add_edge(0, 1, 5)?;
+    graph.add_edge(0, 2, 2)?;
+    graph.add_edge(1, 2, 4)?;
+    graph.add_edge(1, 3, 2)?;
+    graph.add_edge(2, 4, 1)?;
+    graph.add_edge(3, 4, 100)?;
+    
+    let shortest_path_result = graph.shortest_path(0)?;
+
+    println!("Shortest path result {:#?}", shortest_path_result);
     Ok(())
 }    
 
