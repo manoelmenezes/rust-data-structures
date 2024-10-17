94377810ce3d30437dfcd00912672621767b6fd6
Manoel Menezes <manoel.menezes.jr@gmail.com>
2024-10-16 20:17:52 -0700

    Implement Binary Tree.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rds::graph::Graph;
 use rds::graph::weighted::Graph as WeightedGraph;
+use rds::tree::binary_tree::BinaryTree;
+use rds::tree::binary_tree::Node;
 
 fn main() ->Result<(), String> {
     let mut graph = Graph::new(6);
@@ -25,6 +27,38 @@ fn main() ->Result<(), String> {
     let shortest_path_result = graph.shortest_path(0)?;
 
     println!("Shortest path result {:#?}", shortest_path_result);
+
+    let mut bt = BinaryTree::<i32>::new();
+    println!("Is tree empty? {}", bt.is_empty());
+    
+    let root:Node<i32> = Node::complete(
+        10,
+        Box::new(
+            Node::complete(
+                20,
+                Box::new(Node::leaf(15)),
+                Box::new(Node::leaf(25))
+            )
+        ),
+        Box::new(
+            Node::complete(
+                5,
+                Box::new(Node::leaf(1)),
+                Box::new(Node::leaf(8))
+            )
+        ),
+    );
+    bt.set_root(root);
+    println!("Is tree empty? {}", bt.is_empty());
+
+    println!("Contains 10? {}", bt.contains(10));
+    println!("Contains 20? {}", bt.contains(20));
+    println!("Contains 15? {}", bt.contains(15));
+    println!("Contains 25? {}", bt.contains(25));
+    println!("Contains 5? {}", bt.contains(5));
+    println!("Contains 1? {}", bt.contains(1));
+    println!("Contains 8? {}", bt.contains(8));
+    println!("Contains 100? {}", bt.contains(100));
     Ok(())
 }    
 
---
+++ src/tree/binary_tree.rs
@@ -0,0 +1,92 @@
+#[derive(PartialEq)]
+pub struct Node<T> {
+    value: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl<T: std::cmp::PartialEq + Copy> Node<T> {
+
+    pub fn complete(value: T, left: Box<Node<T>>, right: Box<Node<T>>) -> Self {
+        Self {
+            value,
+            left: Some(left),
+            right: Some(right),
+        }
+    } 
+
+    pub fn leaf(value: T) -> Self {
+        Self {
+            value,
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn with_left(value: T, left: Box<Node<T>>) -> Self {
+        Self {
+            value,
+            left: Some(left),
+            right: None,
+        }
+    }
+
+    pub fn with_right(value: T, right: Box<Node<T>>) -> Self {
+        Self {
+            value,
+            left: None,
+            right: Some(right),
+        }
+    }
+
+    pub fn contains(&self, value: T) -> bool {
+        if value == self.value {
+            return true;
+        }
+
+        if let Some(node) = &self.left {
+           if node.contains(value) {
+               return true;
+           }
+        }
+
+        if let Some(node) = &self.right {
+            if  node.contains(value) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+}
+
+pub struct BinaryTree<T> {
+    root: Option<Node<T>>,
+}
+
+impl<T: std::cmp::PartialEq + Copy> BinaryTree<T> {
+
+    pub fn new() -> Self {
+        Self {
+            root: None,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.root == None
+    }
+
+    pub fn set_root(&mut self, node: Node<T>) {
+        self.root = Some(node);
+    }
+
+    pub fn contains(&self, value: T) -> bool {
+        if let Some(node) = &self.root {
+           node.contains(value)
+        } else {
+            false
+        }
+    }
+}
+
