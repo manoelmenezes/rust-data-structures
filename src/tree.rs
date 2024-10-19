f4ce209ee0b0169d93a0f512a931b2e3f2df9847
Manoel Menezes <manoel.menezes.jr@gmail.com>
2024-10-19 00:10:55 -0700

    Add unit tests for tree::Node.

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,7 @@
 pub mod binary_tree;
 pub mod bst;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct Node<T> {
     value: T,
     left: Option<Box<Node<T>>>,
@@ -62,5 +62,76 @@ impl<T: std::cmp::PartialEq + Copy> Node<T> {
         false
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn leaf() {
+        let leaf = Node::<i32>::leaf(1);
+        assert_eq!(leaf.left, None);
+        assert_eq!(leaf.right, None);
+        assert_eq!(leaf.value, 1);
+    }
+
+    #[test]
+    fn with_left() {
+        let node = Node::<i32>::with_left(1, Node{value:0, left:None, right: None});
+        assert_eq!(node.value, 1);
+        assert_eq!(node.right, None);
+
+        let left = node.left.unwrap();
+        assert_eq!(left.value, 0);
+        assert_eq!(left.left, None);
+        assert_eq!(left.right, None);
+    }    
+
+
+    #[test]
+    fn with_right() {
+        let node = Node::<i32>::with_right(1, Node{value:0, left:None, right: None});
+        assert_eq!(node.value, 1);
+        assert_eq!(node.left, None);
+
+        let right = node.right.unwrap();
+        assert_eq!(right.value, 0);
+        assert_eq!(right.left, None);
+        assert_eq!(right.right, None);
+    }
+    
+    #[test]
+    fn complete() {
+        let node = Node::<i32>::complete(
+            0,
+            Node{value:-1, left:None, right: None},
+            Node{value:1, left:None, right: None},
+        );
+        assert_eq!(node.value, 0);
+
+        let left = node.left.unwrap();
+        assert_eq!(left.value, -1);
+        assert_eq!(left.left, None);
+        assert_eq!(left.right, None);
+        
+        let right = node.right.unwrap();
+        assert_eq!(right.value, 1);
+        assert_eq!(right.left, None);
+        assert_eq!(right.right, None);
+    }
+
+    #[test]
+    fn contains() {
+        let node = Node::<i32>::complete(
+            0,
+            Node{value:-1, left:None, right: None},
+            Node{value:1, left:None, right: None},
+        );
+        assert!(node.contains(0));
+        assert!(node.contains(-1));
+        assert!(node.contains(1));
+        assert!(!node.contains(10));
+    }    
 }
 
